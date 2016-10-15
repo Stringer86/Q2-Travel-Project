b1f4429ab1d4782d8dbb88cee4122242bae68774
Stringer86 <stringer.stephen@gmail.com>
2016-10-15 11:15:42 -0700

    created tables in migrations folder

--- migrations/20161014193913_destinations.js.js
+++ migrations/20161014193913_destinations.js.js
@@ -1,8 +1,17 @@
+'use strict'
 
-exports.up = function(knex, Promise) {
-  
+exports.up = function(knex) {
+  return knex.schema.createTable('destinations', (table) => {
+    table.increments();
+    table.string('name').notNullable().defaultTo('');
+    table.text('description').notNullable().defaultTo('');
+    table.boolean('speaks_english').notNullable().defaultTo('');
+    table.text('photo_url').notNullable().defaultTo('');
+    table.string('currency').notNullable().defaultTo('');
+    table.timestamps('true', 'true');
+  });
 };
 
-exports.down = function(knex, Promise) {
-  
+exports.down = function(knex) {
+  return knex.schema.dropTable('destinations');
 };
