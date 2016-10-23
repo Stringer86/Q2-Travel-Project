33793cce0d053aa95678f3381527c63d3958aa88
Stringer86 <stringer.stephen@gmail.com>
2016-10-23 16:05:46 -0700

    index redirect

--- public/js/index.js
+++ public/js/index.js
@@ -36,7 +36,7 @@
 
     $.ajax(options)
       .done(() => {
-        window.location.href = '/user_search.html';
+        window.location.href = '/index.html';
       })
       .fail(($xhr) => {
         Materialize.toast($xhr.responseText, 3000);
